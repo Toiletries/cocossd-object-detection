94c3d14128ff17e087fe62bcc377d9973503a67b
Toiletries <92912011+Toiletries@users.noreply.github.com>
2023-04-19 18:16:09 +1000

    Add files via upload

---
+++ car and human.js
@@ -0,0 +1,30 @@
+img = "";
+
+function preload(){
+  img = loadImage('car and human.png');
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+  
+}
+function modelLoaded(){
+  console.log("Model Loaded!")
+  status= true
+  objectDetector.detect(img,gotResult) 
+
+}
+function gotResult(error, results){
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(results)
+  }
+}
